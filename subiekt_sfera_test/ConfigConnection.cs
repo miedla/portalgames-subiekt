a90cdb379bdb506ea57ae1654e60d55ab4d43441
Lukasz Miedla <lukasz.miedla@gmail.com>
2017-03-23 15:45:35 +0100

    configconnection untracked

--- subiekt_sfera_test/ConfigConnection.cs
+++ subiekt_sfera_test/ConfigConnection.cs
@@ -8,12 +8,12 @@ namespace subiekt_sfera_test
 {
     public static class ConfigConnection
     {
-        public static string ServerGt = "KRYSTIAN\\INSERTGT";
+        public static string ServerGt = "(local)\\INSERTGT";
         public static string BazaGt = "test3";
         public static string OperatorGt = "Szef";
         public static string OperatorGThaslo = "";
-        public static string Uzytkownik = "sa"; 
-        public static string UzytkownikHaslo = "";
+        public static string Uzytkownik = "";//"sa"; 
+        public static string UzytkownikHaslo = "";//"";
 
         public static string PortalGamesServer = "8706.m.tld.pl";
         public static string PortalGamesBaza = "baza8706_11";
